9cabef207c201c25c922384077ca62e1ef8420c0
opeyemi olatunji <roseolatunji93@gmail.com>
2023-06-14 15:51:43 -0700

    data structures

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
+
 def weight_average(my_list=[]):
     if not my_list:
         return 0
 
-    _score = 0
-    _weight = 0
+    n = 0
+    s = 0
 
     for tup in my_list:
-        _score += tup[0] * tup[1]
-        _weight += tup[1]
+        n += tup[0] * tup[1]
+        s += tup[1]
 
-    return (_score / weight)
+    return (n / s)
