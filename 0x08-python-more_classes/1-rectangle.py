dff2dfc6143bfe3bc27c851c684b8e297f92d591
opeyemi olatunji <roseolatunji93@gmail.com>
2023-07-03 15:47:14 -0700

    classes and objects

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+
+"""Defining a rectangle based on 0-rectangle.py"""
+
+
+
+class Rectangle:
+    """class defined"""
+
+    def __init__(self, width=0, height=0):
+        """initializing"""
+        self.height = height
+        self.width = width
+
+
+    @property
+    def height(self):
+        """Creating a property instsnce"""
+        return (self.__height)
+
+    @height.setter
+    def height(self, value):
+        """initializing height setter"""
+        if type(value) is not int:
+            raise typeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+
+
+    @property
+    def width(self):
+        """creating  a property"""
+        return (self.__width)
+
+    @width.setter
+    def width(self, value):
+        """initializing a setter for the width"""
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
---
+++ 0x08-python-more_classes/2-rectangle.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+
+"""A class tat defines a rectangle based on 1-rectangle.py"""
+
+
+class Rectangle:
+    """Creating a class"""
+
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """creating a property instance"""
+        return (self.__width)
+
+    @width.setter
+    def width(self, value):
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+
+        self.__width = value
+
+
+     @property
+    def height(self):
+        """Get/set the current size of the rectangle."""
+        return (self.__size)
+
+    @height.setter
+    def height(self, value):
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+         """Return the current area of the rectangle"""
+         return (self.__width) * (self.__height)
+     def perimeter(self):
+         """Returning the perimeter"""
+         if (self.__width) == 0 or (self.__height) == 0:
+             return 0
+         return (self.__height * 2) * (self.__width * 2)
