d3f25489172e99593f52698db51e91badbefdf6c
Olatunjiopeyemi <roseolatunji93@gmail.com>
2023-09-11 17:44:23 -0700

    welcome to javascript

--- 0x12-javascript-warm_up/0-javascript_is_amazing.js
+++ 0x12-javascript-warm_up/0-javascript_is_amazing.js
@@ -1,4 +1,3 @@
 #!/usr/bin/node
-const myVar = 'JavaScript is amazing';// setting a constant to string
-
-console.log(myVar);// printing javascript is amazing
+const myVar = 'JavaScript is amazing';
+console.log(myVar);
--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -2,8 +2,6 @@
 let var1 = 'C is fun';
 let var2 = 'Python is cool';
 let var3 = 'JavaScript is amazing';
-
-console.log(var1);// printing c is fun
-console.log(var2);// printing python is cool
-console.log(var3);// printing javascript is amazing
-
+console.log(var1);
+console.log(var2);
+console.log(var3);
