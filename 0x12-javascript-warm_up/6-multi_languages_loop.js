6759eccae71c000dc5022c70044e9bd29cb37ce3
Olatunjiopeyemi <roseolatunji93@gmail.com>
2023-09-11 19:03:33 -0700

    introducing js

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const Arr = ["C is fun", "Python is cool", "JavaScript is amazing"];
+const Arr = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 let i = 0;
 while (i < Arr.length) {
   console.log(Arr[i]);
